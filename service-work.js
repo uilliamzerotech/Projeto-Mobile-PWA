1f31e0b8d37d4c413b0f32bb1208bfb8c0284126
Uiiliam <uilliam.fagner@gmail.com>
2024-06-30 12:03:56 -0300

    first commit

---
+++ service-work.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'despesas-viagens-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/bootstrap.min.css',
+  '/js/jquery-3.7.1.min.js',
+  '/js/bootstrap.bundle.min.js'
+  // Adicione todos os recursos que você deseja armazenar em cache
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
